e472b9b0cd3b4c11599e124fb5823717982ee892
Álisson Pereira Ferreira <113950309+alissonpef@users.noreply.github.com>
2024-11-20 15:44:14 -0300

    Update and rename Algoritmo_de_Booth.asm to Arquvivos/Algoritmo_de_Booth.asm

--- Arquvivos/Algoritmo_de_Booth.asm
+++ Arquvivos/Algoritmo_de_Booth.asm
@@ -1,25 +1,25 @@
 ; Professor: Ricardo Bohaczuk Venturelli
 ; Aluno: Alisson Pereira Ferreira
 
-; O algoritmo lê dois números da memória, verifica se eles são menores que 80h e, em seguida, 
-; realiza uma multiplicação usando o algoritmo de Booth, esta por sua vez é realizada 08 por se tratar
-; de números de 8 bytes.
+; O algoritmo lÃª dois nÃºmeros da memÃ³ria, verifica se eles sÃ£o menores que 80h e, em seguida, 
+; realiza uma multiplicaÃ§Ã£o usando o algoritmo de Booth, esta por sua vez Ã© realizada 08 por se tratar
+; de nÃºmeros de 8 bytes.
 
-; Obs: Os números devem ser colocados transformados de Binario para Hexadecimal.
-; Exemplo: -03H x -05 = FDH x FBH, logo esta conversão deve ser realiza para a plotagem dos valores.
+; Obs: Os nÃºmeros devem ser colocados transformados de Binario para Hexadecimal.
+; Exemplo: -03H x -05 = FDH x FBH, logo esta conversÃ£o deve ser realiza para a plotagem dos valores.
 ; Exemplo 2: Caso o valor seja 80H, saira 00H 00H.
 
 .ORG 0000H
 NUMERO_1:
-    LXI H, 4000H        ; Define o endereço de memória do primeiro número
-    MOV A, M            ; Move a memória para o acumulador A
+    LXI H, 4000H        ; Define o endereÃ§o de memÃ³ria do primeiro nÃºmero
+    MOV A, M            ; Move a memÃ³ria para o acumulador A
     CPI 80H             ; Compara o acumulador A com 80H
     JZ  OVERFLOW        ; Pula para OVERFLOW se forem iguais
     MOV B, A            ; Se for menor, move o acumulador A para o B
     JMP NUMERO_2        ; Pula para o NUMERO_2
 
 NUMERO_2:
-    INX H                ; Incrementa o H para o endereço do segundo número
+    INX H                ; Incrementa o H para o endereÃ§o do segundo nÃºmero
     MOV A, M             ; Move o H para o acumulador A
     CPI 80H              ; Compara o acumulador A com 80H
     JZ  OVERFLOW         ; Pula para o OVERFLOW se forem iguais
@@ -38,7 +38,7 @@ LOOP:
     MOV L, A             ; Move o acumulador A para o L
     MOV A, C             ; Move o C para o acumulador A
     ANI 01H              ; Realiza uma AND com o valor 01H
-    RLC                  ; Rotação à esquerda do acumulador A
+    RLC                  ; RotaÃ§Ã£o Ã  esquerda do acumulador A
     ADD L                ; Soma o L com o acumulador A
     CPI 00H              ; Compara o acumulador A com 00H
     JZ  ROTACAO          ; Pula para a ROTACAO se forem iguais
@@ -66,15 +66,15 @@ ROTACAO:
     CMC                  ; Complementa o bit de carry (C)
     MOV A, D             ; Move o D para o acumulador A
     ANI 80H              ; Realiza uma AND com o valor 80H
-    RAL                  ; Rotação à esquerda do acumulador A através do carry
+    RAL                  ; RotaÃ§Ã£o Ã  esquerda do acumulador A atravÃ©s do carry
     MOV A, D             ; Move o D para o acumulador A
-    RAR                  ; Rotação à direita em A através do carry
+    RAR                  ; RotaÃ§Ã£o Ã  direita em A atravÃ©s do carry
     MOV D, A             ; Move o acumulador A para o D
     MOV A, C             ; Move o C para o acumulador A
-    RAR                  ; Rotação à direita em A através do carry
+    RAR                  ; RotaÃ§Ã£o Ã  direita em A atravÃ©s do carry
     MOV C, A             ; Move o acumulador A para o C
     MOV A, E             ; Move o E para o acumulador A
-    RAL                  ; Rotação à esquerda em A através do carry
+    RAL                  ; RotaÃ§Ã£o Ã  esquerda em A atravÃ©s do carry
     ANI 01H              ; Realiza uma AND com o valor 01H
     MOV E, A             ; Move o acumulador A para o E
     DCR H                ; Decrementa o valor de H
@@ -83,16 +83,16 @@ ROTACAO:
 
 OVERFLOW:
     LXI H, 4002H         ; Carrega o par HL com o valor 4002H
-    MVI M, 00H           ; Move o valor 00H para a memória apontada por H
+    MVI M, 00H           ; Move o valor 00H para a memÃ³ria apontada por H
     INX H                ; Incrementa o H
-    MVI M, 00H           ; Move o valor 00H para a memória apontada por L
+    MVI M, 00H           ; Move o valor 00H para a memÃ³ria apontada por L
     JMP FIM              ; Pula para o FIM
 
 RESULTADO:
     LXI H, 4002H         ; Carrega o par HL com o valor 4002H
-    MOV M, C             ; Move o C para a memória apontada por H
+    MOV M, C             ; Move o C para a memÃ³ria apontada por H
     INX H                ; Incrementa o H
-    MOV M, D             ; Move o D para a memória apontada por L
+    MOV M, D             ; Move o D para a memÃ³ria apontada por L
     JMP FIM              ; Pula para o FIM
 
 FIM:
